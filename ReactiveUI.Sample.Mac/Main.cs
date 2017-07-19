eb1219b708c5f38fea19b2f4be99b1b07eafd700
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-07-19 19:14:38 +0900

    add Xamarin.Mac project

---
+++ ReactiveUI.Sample.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace ReactiveUI.Sample.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}

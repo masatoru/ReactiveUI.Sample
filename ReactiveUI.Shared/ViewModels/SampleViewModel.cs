a24003cc5ca0842fb525f5fb58b1f28ee8ac1c91
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-07-19 17:48:54 +0900

    add WPF project and ReactiveUI ViewModel

---
+++ ReactiveUI.Shared/ViewModels/SampleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace ReactiveUI.Shared.ViewModels
+{
+    public class SampleViewModel : ReactiveObject
+    {
+        private string _Text;
+        public string Text
+        {
+            get => _Text;
+            set => this.RaiseAndSetIfChanged(ref _Text, value);
+        }
+
+        public ReactiveCommand<object> Send { get; private set; }
+
+        public SampleViewModel()
+        {
+            // We can only send tweets if we have text less that 140 characters
+            var canSend = this.WhenAny(vm => vm.Text, s => 
+                !String.IsNullOrEmpty(s.Value) && s.Value.Length < 140);
+            Send = ReactiveCommand.Create(canSend);
+
+            // What happens when they push the button
+            Send.Subscribe(_ => Console.WriteLine("Send Tweet: " + Text));
+        }
+    }
+}
